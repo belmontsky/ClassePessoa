d39536cbc43cba697acee04b4519c8801a9dfd30
belmontsky <ferkusanagi@gmail.com>
2023-09-18 12:05:01 -0300

    Adicionar arquivos de projeto.

---
+++ ClassePessoa/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Email { get; set; }
+
+    public Pessoa(string nome, int idade, string email)
+    {
+        Nome = nome;
+        Idade = idade;
+        Email = email;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Pessoa pessoal1 = new Pessoa("Daniel", 30, "danielfernandomo23@gmail.com");
+
+        Console.WriteLine("Nome: " + pessoal1.Nome);
+        Console.WriteLine("Idade: " + pessoal1.Idade);
+        Console.WriteLine("Email: " + pessoal1.Email);
+        Console.WriteLine();
+
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+        
+}
+
